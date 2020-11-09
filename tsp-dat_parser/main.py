3579e2988f32f446aea1ffdd027e65da8fa0253d
vutrang155 <tranghoangphongvu@gmail.com>
2020-11-09 17:44:02 +0100

    nov 9 18h

--- tsp-dat_parser/main.py
+++ tsp-dat_parser/main.py
@@ -1,4 +1,6 @@
 from TSP import TSP
+import numpy as np
+import matplotlib.pyplot as plt
 if __name__ == '__main__':
     filename = 'berlin52'
     m = TSP(filename+'.tsp')
@@ -7,6 +9,20 @@ if __name__ == '__main__':
     n = m.dimension
     D = m.distance_matrix()
 
+    m_np = np.reshape(np.array(m.positions), (-1, 2))
+    x = m_np[:,0]
+    y = m_np[:,1]
+    print(x)
+    plt.plot(x ,y,'bo')
+
+    sol = [1,22,31,18,3,17,21,42,7,2,30,23,20,50,29,16,46,44,34,35,36,39,40,37,38,48,24]
+    sol = list(map(lambda x : x-1, sol))
+    print(sol)
+    for i in sol:
+        plt.plot(m_np[i,0], m_np[i,1], 'r-')
+    plt.show()
+
+'''
     # Write to dat
     f = open(filename+".dat", "w")
     f.write("n = "+str(n)+";\n")
@@ -22,3 +38,4 @@ if __name__ == '__main__':
             f.write(",\n\t")
     f.write("];")
     f.close()
+    '''
