d3b786dff5cab0542062585dc89408574acf7c3e
vutrang155 <tranghoangphongvu@gmail.com>
2020-11-17 09:13:05 +0100

    add RSS

--- Recuit Simule/main.py
+++ Recuit Simule/main.py
@@ -3,15 +3,33 @@
 
 
 from RSD import RSD
-
+from RSS import RSS
+import time
 
 # Parametre algo Recuit simule
 # Nom du fichier contenant les données 
- 
-T0=50.0
-Tmin=1e-3
-tau=1e4
-filename='berlin52'
-traj = RSD(T0,Tmin,tau,filename)
-traj.solve()
+
+if __name__ == "__main__":
+    T0=50.0
+    Tmin=1e-3
+    tau=1e4
+    filename='Data/berlin52'
+
+    t0 = time.time()
+
+    traj = RSD(T0,Tmin,tau,filename)
+    traj.solve()
+
+    t1 = time.time()
+    total = t1-t0
+    print("Resourdre RSD en {0:.1f}s ".format(total))
+
+    t0 = time.time()
+
+    traj = RSS(T0,Tmin,tau,filename)
+    traj.solve()
+
+    t1 = time.time()
+    total = t1-t0
+    print("Resourdre RSS en {0:.1f}s ".format(total))
 
